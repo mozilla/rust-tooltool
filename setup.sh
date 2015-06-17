744904bde8a117ab9ab5d5529a5d5018464a60f0
Ralph Giles <giles@mozilla.com>
2015-06-17 10:13:00 -0700

    Manifest and setup for rust-1.0.0-beta.5 official builds.

--- setup.sh
+++ setup.sh
@@ -1,33 +1,8 @@
 #!/bin/sh
 
-unpack_if_present() {
-  _base=${1%.tar.[gbx]z*}
-  if test -f "$1"; then
-    rm -rf "$_base"
-    tar xf "$1"
-    if test -n "$2"; then
-      test -d "$2" && rm -rf "$2"
-      mv "$_base" "$2"
-    fi
-  fi
-}
-
-unzip_if_present() {
-  _base=${1%.zip}
-  if test -f "$1"; then
-    rm -rf "$_base"
-    unzip "$1"
-    if test -n "$2"; then
-      test -d "$2" && rm -rf "$2"
-      mv "$_base" "$2"
-    fi
-  fi
-}
-
-unpack_if_present gcc.tar.xz
-unpack_if_present clang.tar.bz2
-unpack_if_present sccache.tar.bz2
-
-unzip_if_present LLVM-*-win32.zip LLVM
-
-unpack_if_present rust-*.tar.gz rustc
+rm -rf gcc
+tar -Jxf gcc.tar.xz
+rm -rf sccache
+tar -jxf sccache.tar.bz2
+rm -rf rustc
+tar xf rust-1.0.0-x86_64-unknown-linux-gnu.tar.gz --strip-components=1
